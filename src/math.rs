a77d2001e9cb5ea61ad4fd7d5a77f9128267759d
Hawo Höfer <hawo.hoefer@kit.edu>
2025-06-05 09:54:36 +0200

    add synchrotron function computation
    ported from acm757 fortran implementation

--- src/math.rs
+++ src/math.rs
@@ -6,6 +6,12 @@ pub const H_EV_S: f64 = 4.135_667_696e-15f64;
 /// speed of light in m / s
 pub const C_M_S: f64 = 299_792_485.0f64;
 
+/// vacuum permeability in N A^-2
+pub const MU_0_N_A2: f64 = 1.25663706127e-6;
+
+/// vacuum permittivity in F m^-1
+pub const EPS_0_F_M1: f64 = 8.8541788188e-12;
+
 pub fn e_kev_to_lambda_ams(e_kev: f64) -> f64 {
     // e = h * c / lambda
     // lambda = h * c / e
@@ -76,4 +82,399 @@ pub fn pseudo_voigt(dx: f32, eta: f32, fwhm: f32) -> f32 {
     let sigma = (1.0 / two_sqrt_ln_2) * fwhm;
     let gamma = fwhm / 2.0;
     eta * lorentz(dx, gamma) + (1.0 - eta) * gauss(dx, sigma)
+}
+
+pub mod acm757 {
+    /// Evaluate a chebyshev series using the Clenshaw method with Reinsch modification,
+    /// as analyzed in the paper by Oliver. This function is adapted from
+    /// Algorithm 757: MISCFUN, a software package to compute uncommon special functions.
+    /// DOI: https://doi.org/10.1145/232826.232846
+    ///
+    /// Original Author:
+    ///     Dr. Allan J. MacLeod,
+    ///     Dept. of Mathematics and Statistics,
+    ///     University of Paisley,
+    ///     High St.,
+    ///     PAISLEY,
+    ///     SCOTLAND
+    ///
+    /// Reference:
+    ///     "An error analysis of the modified Clenshaw method for evaluating
+    ///      Chebyshev and Fourier series" J. Oliver,
+    ///      J.I.M.A., vol. 20, 1977, pp379-391
+    ///
+    /// * `coef`: chebyshev coefficients
+    /// * `t`: value to evaluate function at
+    pub fn cheval(coef: &[f64], t: f64) -> f64 {
+        let mut u1 = 0.0;
+        let mut u2 = 0.0;
+
+        if t.abs() < 0.6 {
+            // If ABS ( T )  < 0.6 use the standard Clenshaw method
+            let mut u0 = 0.0; // U0 = ZERO
+            let tt = 2.0 * t; // TT = T + T
+                              // DO 100 I = N , 0 , -1
+            for a_i in coef.iter().rev() {
+                u2 = u1; // U2 = U1
+                u1 = u0; // U1 = U0
+                         // U0 = TT * U1 + A( I ) - U2
+                u0 = tt * u1 + a_i - u2;
+            }
+            // CHEVAL =  ( U0 - U2 ) / TWO
+            return (u0 - u2) / 2.0;
+        }
+
+        // If ABS ( T )  > =  0.6 use the Reinsch modification
+        let mut d1 = 0.0;
+        let mut d2 = 0.0;
+        // IF ( T .GT. ZERO ) THEN
+        if t > 0.0 {
+            // TT =  ( T - HALF ) - HALF
+            // TT = TT + TT
+            let tt = 2.0 * (t - 1.0);
+            // DO 200 I = N , 0 , -1
+            for a_i in coef.iter().rev() {
+                d2 = d1; // D2 = D1
+                u2 = u1; // U2 = U1
+                         // D1 = TT * U2 + A( I ) + D2
+                d1 = tt * u2 + a_i + d2;
+                // U1 = D1 + U2
+                u1 = d1 + u2;
+            }
+            // CHEVAL =  ( D1 + D2 ) / TWO
+            return (d1 + d2) / 2.0;
+        }
+
+        // t <= -0.6
+        // TT =  ( T + HALF ) + HALF
+        // TT = TT + TT
+        let tt = 2.0 * (t + 1.0);
+        // DO 300 I = N , 0 , -1
+        for a_i in coef.iter().rev() {
+            d2 = d1; // D2 = D1
+            u2 = u1; // U2 = U1
+                     // D1 = TT * U2 + A( I ) - D2
+            d1 = tt * u2 + a_i - d2;
+            // U1 = D1 - U2
+            u1 = d1 - u2;
+        }
+
+        // CHEVAL =  ( D1 - D2 ) / TWO
+        return (d1 - d2) / 2.0;
+    }
+
+    /// Calculate the first synchrotron radiation function defined as
+    ///    SYNCH1(x) = x * Integral{x to inf} K(5/3)(t) dt,
+    /// where K(5/3) is a modified Bessel function of order 5/3.
+    /// The code uses Chebyshev expansions, the coefficients of which
+    /// are given to 20 decimal places.
+    ///
+    /// The function is undefined for x < 0, and returns None in that case.
+    ///
+    /// This function is adapted from
+    /// Algorithm 757: MISCFUN, a software package to compute uncommon special functions.
+    /// DOI: https://doi.org/10.1145/232826.232846
+    ///
+    /// Original Author:
+    ///     Dr. Allan J. MacLeod,
+    ///     Dept. of Mathematics and Statistics,
+    ///     University of Paisley,
+    ///     Paisley,
+    ///     SCOTLAND
+    ///     PA1 2BE
+    ///
+    /// * `x`: function input
+    pub fn synch_1(x: f64) -> Option<f64> {
+        // chebyshev coefficients
+        const ASYNC1: [f64; 14] = [
+            30.36468_29825_01076_27340e0,
+            17.07939_52774_08394_57449e0,
+            4.56013_21335_45072_88887e0,
+            0.54928_12467_30419_97963e0,
+            0.37297_60750_69301_1724e-1,
+            0.16136_24302_01041_242e-2,
+            0.48191_67721_20370_7e-4,
+            0.10512_42528_89384e-5,
+            0.17463_85046_697e-7,
+            0.22815_48654_4e-9,
+            0.24044_3082e-11,
+            0.20865_88e-13,
+            0.15167e-15,
+            0.94e-18,
+        ];
+        const ASYNC2: [f64; 12] = [
+            0.44907_21623_53266_08443e0,
+            0.89835_36779_94187_2179e-1,
+            0.81044_57377_21512_894e-2,
+            0.42617_16991_08916_19e-3,
+            0.14760_96312_70746_0e-4,
+            0.36286_33615_3998e-6,
+            0.66634_80749_84e-8,
+            0.94907_71655e-10,
+            0.10791_2491e-11,
+            0.10022_01e-13,
+            0.7745e-16,
+            0.51e-18,
+        ];
+        const ASYNCA: [f64; 25] = [
+            2.13293_05161_35500_09848e0,
+            0.74135_28649_54200_2401e-1,
+            0.86968_09990_99641_978e-2,
+            0.11703_82624_87756_921e-2,
+            0.16451_05798_61919_15e-3,
+            0.24020_10214_20640_3e-4,
+            0.35827_75638_93885e-5,
+            0.54477_47626_9837e-6,
+            0.83880_28561_957e-7,
+            0.13069_88268_416e-7,
+            0.20530_99071_44e-8,
+            0.32518_75368_8e-9,
+            0.51791_40412e-10,
+            0.83002_9881e-11,
+            0.13352_7277e-11,
+            0.21591_498e-12,
+            0.34996_73e-13,
+            0.56994_2e-14,
+            0.92906e-15,
+            0.15222e-15,
+            0.2491e-16,
+            0.411e-17,
+            0.67e-18,
+            0.11e-18,
+            0.2e-19,
+        ];
+
+        // f64-specific constants
+        //
+        // let EPSNEG: f64 = 2.0f64.powi(-53);
+        // let XLOW: f64 = (8.0 * EPSNEG);
+        const XLOW: f64 = 2.9802322387695313e-8;
+        // let XHIGH1 = -8.0 * std::f64::MIN_POSITIVE.ln() / 7.0;
+        const XHIGH1: f64 = 8.095959068940161e2;
+        // let XHIGH2 = std::f64::MIN_POSITIVE.ln();
+        const XHIGH2: f64 = -7.083964185322641e2;
+
+        // numeric constants
+        const CONLOW: f64 = 2.14952_82415_34478_63671;
+        const LNRTP2: f64 = 0.22579_13526_44727_43236;
+        const PIBRT3: f64 = 1.81379_93642_34217_85059;
+
+        if x < 0.0 {
+            // undefined for x < 0
+            return None;
+        }
+
+        // IF ( X .LE. FOUR ) THEN
+        if x <= 4.0 {
+            // Code for 0 <= x <= 4
+            // XPOWTH = X ** ( ONE / THREE )
+            let xpowth = x.powf(1.0 / 3.0);
+            // IF ( X .LT. XLOW ) THEN
+            if x < XLOW {
+                // SYNCH1 = CONLOW * XPOWTH
+                return Some(CONLOW * xpowth);
+            }
+
+            // T = ( X * X / EIGHT - HALF ) - HALF
+            let mut t = x.powi(2) / 8.0 - 1.0;
+            // CHEB1 = CHEVAL(NTERM1,ASYNC1,T)
+            // CHEB2 = CHEVAL(NTERM2,ASYNC2,T)
+            let cheb1 = cheval(&ASYNC1, t);
+            let cheb2 = cheval(&ASYNC2, t);
+            // T = XPOWTH * CHEB1 - ( XPOWTH**11 ) * CHEB2
+            t = xpowth * cheb1 - xpowth.powi(11) * cheb2;
+            // SYNCH1 = T - PIBRT3 * X
+            return Some(t - PIBRT3 * x);
+        }
+
+        // IF ( X .GT. XHIGH1 ) THEN
+        if x > XHIGH1 {
+            // SYNCH1 = ZERO
+            return Some(0.0);
+        }
+
+        // T = ( TWELVE - X ) / ( X + FOUR )
+        let mut t = (12.0 - x) / (x + 4.0);
+        // CHEB1 = CHEVAL(NTERM3,ASYNCA,T)
+        let cheb1 = cheval(&ASYNCA, t);
+        // T = LNRTP2 - X + LOG( SQRT(X) * CHEB1 )
+        t = LNRTP2 - x + (x.sqrt() * cheb1).ln();
+
+        // IF ( T .LT. XHIGH2 ) THEN
+        if t < XHIGH2 {
+            // SYNCH1 = ZERO
+            return Some(0.0);
+        }
+
+        // SYNCH1 = EXP(T)
+        return Some(t.exp());
+    }
+
+    /// Compute the second synchrotron radiation function defined as
+    ///          SYNCH2(x) = x * K(2/3)(x)
+    /// where K(2/3) is a modified Bessel function of order 2/3.
+    /// The code uses Chebyshev expansions, the coefficients of which
+    /// are given to 20 decimal places.
+    ///
+    /// The function is undefined for x < 0, and returns None in that case.
+    ///
+    /// This function is adapted from
+    /// Algorithm 757: MISCFUN, a software package to compute uncommon special functions.
+    /// DOI: https://doi.org/10.1145/232826.232846
+    ///
+    /// Original Author:
+    ///     Dr. Allan J. MacLeod,
+    ///     Dept. of Mathematics and Statistics,
+    ///     University of Paisley,
+    ///     Paisley,
+    ///     SCOTLAND
+    ///     PA1 2BE
+    ///
+    /// * `x`: synchrotron function argument
+    pub fn synch_2(x: f64) -> Option<f64> {
+        // chebyshev coefficients
+        const ASYN21: [f64; 15] = [
+            38.61783_99238_43085_48014e0,
+            23.03771_55949_63734_59697e0,
+            5.38024_99868_33570_59676e0,
+            0.61567_93806_99571_07760e0,
+            0.40668_80046_68895_5843e-1,
+            0.17296_27455_26484_141e-2,
+            0.51061_25883_65769_9e-4,
+            0.11045_95950_22012e-5,
+            0.18235_53020_649e-7,
+            0.23707_69803_4e-9,
+            0.24887_2963e-11,
+            0.21528_68e-13,
+            0.15607e-15,
+            0.96e-18,
+            0.1e-19,
+        ];
+        const ASYN22: [f64; 14] = [
+            7.90631_48270_66080_42875e0,
+            3.13534_63612_85342_56841e0,
+            0.48548_79477_45371_45380e0,
+            0.39481_66758_27237_2337e-1,
+            0.19661_62233_48088_022e-2,
+            0.65907_89322_93042_0e-4,
+            0.15857_56134_98559e-5,
+            0.28686_53011_233e-7,
+            0.40412_02359_5e-9,
+            0.45568_4443e-11,
+            0.42045_90e-13,
+            0.32326e-15,
+            0.210e-17,
+            0.1e-19,
+        ];
+        const ASYN2A: [f64; 19] = [
+            2.02033_70941_70713_60032e0,
+            0.10956_23712_18074_0443e-1,
+            0.85423_84730_11467_55e-3,
+            0.72343_02421_32822_2e-4,
+            0.63124_42796_26992e-5,
+            0.56481_93141_1744e-6,
+            0.51283_24801_375e-7,
+            0.47196_53291_45e-8,
+            0.43807_44214_3e-9,
+            0.41026_81493e-10,
+            0.38623_0721e-11,
+            0.36613_228e-12,
+            0.34802_32e-13,
+            0.33301_0e-14,
+            0.31856e-15,
+            0.3074e-16,
+            0.295e-17,
+            0.29e-18,
+            0.3e-19,
+        ];
+
+        // f64-specific constants
+        //
+        // let EPSNEG: f64 = 2.0f64.powi(-53);
+        // let XLOW: f64 = (8.0 * EPSNEG);
+        const XLOW: f64 = 2.9802322387695313e-8;
+        // let XHIGH1 = -8.0 * std::f64::MIN_POSITIVE.ln() / 7.0;
+        const XHIGH1: f64 = 8.095959068940161e2;
+        // let XHIGH2 = std::f64::MIN_POSITIVE.ln();
+        const XHIGH2: f64 = -7.083964185322641e2;
+
+        // numeric constants
+        const CONLOW: f64 = 1.07476_41207_67239_31836;
+        const LNRTP2: f64 = 0.22579_13526_44727_43236;
+
+        // IF ( X .LE. FOUR ) THEN
+        if x <= 4.0 {
+            // Code for 0 <= x <= 4
+            // XPOWTH = X ** ( ONE / THREE )
+            let xpowth = x.powf(1.0 / 3.0);
+
+            // IF ( X .LT. XLOW ) THEN
+            if x < XLOW {
+                // SYNCH2 = CONLOW * XPOWTH
+                return Some(CONLOW * xpowth);
+            } else {
+                // T = ( X * X / EIGHT - HALF ) - HALF
+                let t = x.powi(2) / 8.0 - 1.0;
+                // CHEB1 = CHEVAL(NTERM1,ASYN21,T)
+                // CHEB2 = CHEVAL(NTERM2,ASYN22,T)
+                let cheb1 = cheval(&ASYN21, t);
+                let cheb2 = cheval(&ASYN22, t);
+
+                // SYNCH2 = XPOWTH * CHEB1 - ( XPOWTH**5 ) * CHEB2
+                return Some(xpowth * cheb1 - xpowth.powi(5) * cheb2);
+            }
+        } else {
+            // IF ( X .GT. XHIGH1 ) THEN
+            if x > XHIGH1 {
+                // SYNCH2 = ZERO
+                return Some(0.0);
+            } else {
+                // T = ( TEN - X ) / ( X + TWO )
+                let mut t = (10.0 - x) / (x + 2.0);
+                // CHEB1 = CHEVAL(NTERM3,ASYN2A,T)
+                let cheb1 = cheval(&ASYN2A, t);
+                // T = LNRTP2 - X + LOG( SQRT(X) * CHEB1 )
+                t = LNRTP2 - x + (x.sqrt() * cheb1).ln();
+
+                // IF ( T .LT. XHIGH2 ) THEN
+                if x < XHIGH2 {
+                    // SYNCH2 = ZERO
+                    return Some(0.0);
+                } else {
+                    // SYNCH2 = EXP(T)
+                    return Some(t.exp());
+                }
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+        // TODO: add proper testing here - not sure how to do that sensibly?
+
+        #[test]
+        fn sych_1_range() {
+            let n = 1024;
+            let (x0, x1) = (0.0, 10.0);
+            for x in (0..n).map(|x| {
+                let t = (x as f64) / (n - 1) as f64;
+                x0 + (x1 - x0) * t
+            }) {
+                println!("{}, {}", x, synch_1(x).unwrap())
+            }
+        }
+
+        #[test]
+        fn sych_2_range() {
+            let n = 1024;
+            let (x0, x1) = (0.0, 10.0);
+            for x in (0..n).map(|x| {
+                let t = (x as f64) / (n - 1) as f64;
+                x0 + (x1 - x0) * t
+            }) {
+                println!("{}, {}", x, synch_2(x).unwrap())
+            }
+        }
+    }
 }
